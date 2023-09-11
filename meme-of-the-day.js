a475344fa2633c0876311388166104108959e3f0
AwesomeCoder26 <126491929+AwesomeCoder26@users.noreply.github.com>
2023-09-11 12:40:46 -0400

    Update meme-of-the-day.js

--- meme-of-the-day.js
+++ meme-of-the-day.js
@@ -259,14 +259,19 @@ function displayDislikes() {
 
 function sendComment() {
     let comment = document.querySelector("textarea").value;
-    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     if (!signedIn) {
         alert("Sign in to comment...");
         return;
     }
 
+    if (comment == "" || comment == null || comment == undefined){
+        return;
+    }
+
     let uid = auth.currentUser.uid;
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
 
     db.ref("meme-comments").get()
         .then((node) => {
